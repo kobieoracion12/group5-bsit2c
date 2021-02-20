249da4a551d4b24e41ee31205feb4945764f5971
kobieoracion12 <kobie.oracion12@gmail.com>
2021-02-20 19:18:04 +0800

    Final Project

---
+++ src/main/java/com/oopclass/breadapp/views/FxmlView.java
@@ -0,0 +1,45 @@
+package com.oopclass.breadapp.views;
+
+import java.util.ResourceBundle;
+
+/**
+ * OOP Class 20-21
+ *
+ * @author Gerald Villaran
+ */
+public enum FxmlView {
+    
+    LOGIN {
+        @Override
+        public String getTitle() {
+            return getStringFromResourceBundle("user.title");
+        }
+
+        @Override
+        public String getFxmlFile() {
+            return "/fxml/Login.fxml";
+        }
+    },
+
+    USER {
+        @Override
+        public String getTitle() {
+            return getStringFromResourceBundle("user.title");
+        }
+
+        @Override
+        public String getFxmlFile() {
+            return "/fxml/User.fxml";
+        }
+    };
+
+
+    public abstract String getTitle();
+
+    public abstract String getFxmlFile();
+
+    String getStringFromResourceBundle(String key) {
+        return ResourceBundle.getBundle("Bundle").getString(key);
+    }
+
+}
